84f0d6437039c1598fd6d7fe42c441c2908d36cc
rakib-molla <rakibmail1997@gmail.com>
2024-02-18 20:17:45 +0600

    refresh token not set cookie

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,86 @@
+import { Button, Checkbox, Form, Input } from "antd";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 const Login = () => {
-   return (
-      <div>
-         login
+  const [login, { data, error }] = useLoginMutation();
+
+  console.log("data =>", data);
+  console.log("error =>", error);
+
+  const onFinish = (values: any) => {
+    const userInfo = {
+      id: values.id,
+      password: values.password,
+    };
+    login(userInfo);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  type FieldType = {
+    id?: string;
+    password?: string;
+    remember?: string;
+  };
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{ background: "pink", borderRadius: "20px", padding: "10px" }}
+      >
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 80 }}
+          style={{ width: 600 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="Id"
+            name="id"
+            rules={[{ required: true, message: "Please input your ID!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
-   );
+    </div>
+  );
 };
 
 export default Login;
