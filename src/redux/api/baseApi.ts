465f862415772f0310d9468bf7a522fbdd0a47f6
rakib-molla <rakibmail1997@gmail.com>
2024-02-20 04:29:18 +0600

    typescritp error solve

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,4 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+import { BaseQueryApi, BaseQueryFn, DefinitionType, FetchArgs, createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout, setUser } from "../features/auth/authSlice";
 
@@ -14,7 +15,7 @@ const baseQuery = fetchBaseQuery({
   }
 })
 
-const baseQueryWithRefreshToken = async (args, api, extraOptions)=>{
+const baseQueryWithRefreshToken : BaseQueryFn<FetchArgs, BaseQueryApi,DefinitionType> = async (args, api, extraOptions): Promise<any>=>{
   let result = await baseQuery(args, api, extraOptions);
   console.log(result);
   if(result?.error?.status == 401){
