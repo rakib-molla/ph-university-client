ee21192ad67d7acd012a1bb01747468e24eba1b2
rakib-molla <rakibmail1997@gmail.com>
2024-02-25 02:23:04 +0600

    Show Semester Data Using Ant Design Table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,14 +1,88 @@
 import { useGetAllSemesterQuery } from "../../../redux/features/admin/academicManagementApi";
 
-
+import { Table } from 'antd';
+import type { TableColumnsType, TableProps } from 'antd';
 
 const AcademicSemester = () => {
-   const {data} = useGetAllSemesterQuery(undefined);
-   console.log(data);
+   const {data: semesterData} = useGetAllSemesterQuery(undefined);
+   
+   const tableData = semesterData?.data?.map(({_id, name, year, startMonth, endMonth}) =>({
+      _id,
+      name,
+      year,
+      startMonth,
+      endMonth,
+   }))
+
+   
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
+
+const columns: TableColumnsType<DataType> = [
+   {
+      title: 'Name',
+      dataIndex: 'name',
+      filters: [
+        {
+          text: 'Joe',
+          value: 'Joe',
+        },
+        {
+          text: 'Jim',
+          value: 'Jim',
+        },
+        {
+          text: 'Submenu',
+          value: 'Submenu',
+          children: [
+            {
+              text: 'Green',
+              value: 'Green',
+            },
+            {
+              text: 'Black',
+              value: 'Black',
+            },
+          ],
+        },
+      ],
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value: string, record) => record.name.indexOf(value) === 0,
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ['descend'],
+    },
+  {
+    title: 'Year',
+    dataIndex: 'year',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: 'Start Month',
+    dataIndex: 'startMonth',
+  },
+  {
+    title: 'End Month',
+    dataIndex: 'endMonth',
+  },
+];
+
+
+const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+  console.log('params', pagination, filters, sorter, extra);
+  console.log(filters);
+};
+
+
+
    return (
-      <div>
-         AcademicSemester component 
-      </div>
+      <Table columns={columns} dataSource={tableData} onChange={onChange} />
    );
 };
 
