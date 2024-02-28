702c109091638797835e2c2a76cd7dad60cdf7fd
rakib-molla <rakibmail1997@gmail.com>
2024-02-28 10:43:47 +0600

    get all academic faculty

--- src/pages/admin/academicManagement/AcademicFaculty.tsx
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -1,10 +1,75 @@
+import { useState } from "react";
+import { useGetAllAcademicFacultyQuery } from "../../../redux/features/admin/academicFacultyManagementApi";
+import { TQueryParm } from "../../../types/global";
+import { TAcademicSemester } from "../../../types/academicManagement.type";
+import { Button, Table, TableColumnsType, TableProps } from "antd";
 
 
 const AcademicFaculty = () => {
+   const [params, setParams] = useState<TQueryParm[] | undefined>([]);
+   const {data: allFacultyData, isFetching} = useGetAllAcademicFacultyQuery(params);
+   
+   const tableData = allFacultyData?.data?.map(({_id, name, }) =>({
+      key:_id,
+      name,
+     
+   }))
+
+   
+ type TTableData = Pick<TAcademicSemester,  "name" >
+
+const columns: TableColumnsType<TTableData> = [
+   {
+      title: 'Name',
+      dataIndex: 'name',
+      filters: [
+        {
+          text: 'EEE',
+          value: 'EEE',
+        },
+        
+       
+      ],
+    
+    },
+  
+  
+  {
+    title: 'Action',
+    dataIndex: 'x',
+    render: ()=>{
+      return (
+         <div>
+            <Button>Update</Button>
+         </div>
+      )
+    }
+  },
+];
+
+
+const onChange: TableProps<TTableData>['onChange'] = (
+   _pagination,
+   filters,
+   _sorter,
+   extra) => {
+   if(extra.action === 'filter'){
+      const queryParams: TQueryParm[] = [];
+
+      filters.name?.forEach((item)=> 
+      queryParams.push({name: 'name', value: item})
+      );
+      filters.year?.forEach((item)=> 
+      queryParams.push({name: 'year', value: item})
+      );
+
+      setParams(queryParams);
+   }
+};
+
+
    return (
-      <div>
-         AcademicFaculty
-      </div>
+      <Table loading={isFetching} columns={columns} dataSource={tableData} onChange={onChange} />
    );
 };
 
